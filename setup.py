791f4bf6b7c58b42de453b97b0b0c7c0525a9c11
DavidTitoInfantas <dtitoin10@gmail.com>
2025-01-27 07:34:47 -0300

    commit setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='David Tito',
+    author_email='dtitoin10@gmail.com',
+    packages=find_packages()
+    
+)
