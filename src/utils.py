8ce22ff3c0788d177be94ec1503809a8d9ffd88a
DavidTitoInfantas <dtitoin10@gmail.com>
2025-02-23 18:34:33 -0300

    tunning hyperparameters

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 import dill
@@ -24,12 +25,45 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-# def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = models[model_name]  #list(models.values())[i]
+            para= param[model_name]    ##param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=5) #, n_jobs=-1, verbose=verbose, refit=refit)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)
+
+            # Making predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Evaluate model
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+# def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 #     try:
 #         report = {}
 # 
-#         for i in range(len(list(models))):
-#             model = list(models.values())[i]
+#         # for i in range(len(list(models))):
+#         for name_dic, obj_dic in models.items():    
+#             model = obj_dic
 #             model.fit(X_train, y_train)
 # 
 #             # Making predictions
@@ -40,33 +74,9 @@ def save_object(file_path, obj):
 #             train_model_score = r2_score(y_train, y_train_pred)
 #             test_model_score = r2_score(y_test, y_test_pred)
 # 
-#             report[list(models.keys())[i]]=test_model_score
+#             report[name_dic]=test_model_score
 # 
 #         return report
 #     
 #     except Exception as e:
 #         raise CustomException(e, sys)
-    
-def evaluate_models(X_train, y_train, X_test, y_test, models):
-    try:
-        report = {}
-
-        # for i in range(len(list(models))):
-        for name_dic, obj_dic in models.items():    
-            model = obj_dic
-            model.fit(X_train, y_train)
-
-            # Making predictions
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-
-            # Evaluate model
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[name_dic]=test_model_score
-
-        return report
-    
-    except Exception as e:
-        raise CustomException(e, sys)
