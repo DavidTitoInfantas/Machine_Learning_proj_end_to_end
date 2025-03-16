a4deee5d984e01aaff547f6be9aa7537a6cd9aa9
DavidTitoInfantas <dtitoin10@gmail.com>
2025-03-16 18:23:40 -0300

    set port on API

--- application.py
+++ application.py
@@ -42,6 +42,6 @@ def predict_datapoint():
 
 if __name__=='__main__':
     port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0") ##, port=port, debug=True)
     ## Port is set to http://127.0.0.1:5000/
 
